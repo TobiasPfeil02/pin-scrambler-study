07ee5fef2a4b1f47533476a405f12b954003859a
Tobias Pfeil <tobias.pfeil02@gmail.com>
2024-12-31 12:20:25 +0100

    Fix tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-
-    </>
+    <main className="w-full flex flex-col justify-center items-center my-12">
+      <h1 className="text-3xl">Pin Scrambler User Study</h1>
+    </main>
   )
 }
 
